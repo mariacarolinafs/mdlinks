33b8ed2fdfeecf9a7b33933b40ef061143164d7e
mariacarolinafs <mariacarolina_fs@hotmail.com>
2019-11-28 16:43:02 -0200

    function mdlinks fixed

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
-# mdlinks
+Uma comunidade open source nos propôs criar uma ferramenta, usando 
+[Node.js](https://nodejs.org/), que leia e analise arquivos no formato [Node.js](http://nodejs.org/), que leia e analise arquivos no formato links e mostrar algumas
--- cli.js
+++ cli.js
@@ -1,1 +1,10 @@
-// chamar a função mdlinks para ser executada pela linha de comando
+#!/usr/bin/env node
+const mdLinks = require('./lib/index.js')
+
+const path = process.argv[2]
+
+console.log(process.argv);
+
+mdLinks(path)
+.then(result => console.log(result))
+.catch(error => console.log(error))
