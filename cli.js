cc06919e756609f7f5869a96d18c9206c3f7a319
mariacarolinafs <mariacarolina_fs@hotmail.com>
2019-11-29 20:31:40 -0200

    function working and passed tests

--- cli.js
+++ cli.js
@@ -3,8 +3,6 @@ const mdLinks = require('./lib/index.js')
 
 const path = process.argv[2]
 
-console.log(process.argv);
-
 mdLinks(path)
 .then(result => console.log(result))
 .catch(error => console.log(error))
